272fae2dd4424b539b37a4547bc360b575e8d0ab
MartinDanielCP <MartinCalderon92@outlook.com>
2021-04-21 15:08:17 -0300

    Add express serve.

--- src/server.js
+++ src/server.js
@@ -46,7 +46,16 @@ app.use('/seguidos', seguidosRoutes);
 app.use('/secciones', seccionesRoutes);
 app.use('/autores', autoresRoutes);
 
-app.get("/");
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 app.listen(process.env.PORT || 8888, ()=>{
     console.log('Escuchando...')
