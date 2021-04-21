b6c7ba405db2f7b0bd17ab1b63767958882789a6
MartinDanielCP <MartinCalderon92@outlook.com>
2021-04-21 15:01:41 -0300

    Delete static folder.

--- src/server.js
+++ src/server.js
@@ -24,14 +24,6 @@ app.use(fileUpload());
 
 app.use('*/images', express.static('public/images'));
 
-if(process.env.NODE_ENV === 'production'){
-    //set static folder
-    app.use(express.static('client/build'));
-}
-app.get('*',(req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
-
 app.use(cors({
     credentials: true,
     origin: 'http://localhost:3000',
