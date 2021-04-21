7a31037931f9cf44e05c71e13f8c1d4f6295e28c
MartinDanielCP <MartinCalderon92@outlook.com>
2021-04-21 14:56:57 -0300

    Change static folder.

--- src/server.js
+++ src/server.js
@@ -22,12 +22,12 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(fileUpload());
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
 }
-
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
 app.use('*/images', express.static('public/images'));
