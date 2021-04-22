8352d35d41bd6cb2c2b54a2a9888f135062ffe48
MartinDanielCP <MartinCalderon92@outlook.com>
2021-04-22 12:55:29 -0300

    Change CORS for GitHub.

--- src/server.js
+++ src/server.js
@@ -26,7 +26,7 @@ app.use('*/images', express.static('public/images'));
 
 app.use(cors({
     credentials: true,
-    origin: 'https://caba-diario-frontend.vercel.app/',
+    origin: 'https://martindanielcp.github.io/CABA-Diario-Frontend/',
     allowedHeaders: ['Content-Type'],
 }));
 
