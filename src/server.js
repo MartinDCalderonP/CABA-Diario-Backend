1ce02af0bf0b6cf35c640d58c178baccfc75fd87
MartinDanielCP <MartinCalderon92@outlook.com>
2021-04-22 22:12:08 -0300

    One CORS.

--- src/server.js
+++ src/server.js
@@ -26,7 +26,7 @@ app.use('*/images', express.static('public/images'));
 
 app.use(cors({
     credentials: true,
-    origin: ['https://martindanielcp.github.io', 'http://localhost:3000/'],
+    origin: 'https://martindanielcp.github.io',
     allowedHeaders: ['Access-Control-Allow-Origin', 'Content-Type'],
 }));
 
