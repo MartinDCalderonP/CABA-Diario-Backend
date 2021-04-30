649c54516c19d565b5033d4321d19c0834baf8a0
MartinDanielCP <MartinCalderon92@outlook.com>
2021-04-29 22:45:24 -0300

    Minor bug fix.

--- src/routes/notas_routes.js
+++ src/routes/notas_routes.js
@@ -327,7 +327,7 @@ router.post('/', upload, (req, res)=>{
         const params = {
             Bucket: process.env.AWS_BUCKET_NAME,
             Key: 'public/images/newsImages/' + Date.now() + fileType,
-            Body: req.file.buffer,
+            Body: req.files.Imagen.data,
             ACL: 'public-read'
         }
 
